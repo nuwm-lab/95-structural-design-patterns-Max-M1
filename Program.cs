79d06bec91dd897eca0ddd63c2255f8718097c5a
Max <mmaslov848@gmail.com>
2024-12-07 02:56:15 +0200

    Added 10 lab work

--- Program.cs
+++ Program.cs
@@ -1,18 +1,73 @@
 ﻿using System;
 
-namespace LabWork
+namespace AdapterPatternExample
 {
-    // Даний проект є шаблоном для виконання лабораторних робіт
-    // з курсу "Об'єктно-орієнтоване програмування та патерни проектування"
-    // Необхідно змінювати і дописувати код лише в цьому проекті
-    // Відео-інструкції щодо роботи з github можна переглянути 
-    // за посиланням https://www.youtube.com/@ViktorZhukovskyy/videos 
+    public interface INewJoystick
+    {
+        void ConnectNewJoystick();
+        void PlayGameWithNewJoystick();
+    }
+
+    public class NewJoystick : INewJoystick
+    {
+        public void ConnectNewJoystick()
+        {
+            Console.WriteLine("Новий джойстик пiдключено.");
+        }
+
+        public void PlayGameWithNewJoystick()
+        {
+            Console.WriteLine("Граємо за допомогою нового джойстика.");
+        }
+    }
+
+    public class OldJoystick
+    {
+        public void ConnectOldJoystick()
+        {
+            Console.WriteLine("Старий джойстик пiдключено.");
+        }
+
+        public void PlayGameWithOldJoystick()
+        {
+            Console.WriteLine("Граємо за допомогою старого джойстика.");
+        }
+    }
+
+    public class JoystickAdapter : INewJoystick
+    {
+        private readonly OldJoystick _oldJoystick;
+
+        public JoystickAdapter(OldJoystick oldJoystick)
+        {
+            _oldJoystick = oldJoystick;
+        }
+
+        public void ConnectNewJoystick()
+        {
+            _oldJoystick.ConnectOldJoystick();
+        }
+
+        public void PlayGameWithNewJoystick()
+        {
+            _oldJoystick.PlayGameWithOldJoystick();
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            
-            Console.WriteLine("Hello World!");
+            INewJoystick newJoystick = new NewJoystick();
+            newJoystick.ConnectNewJoystick();
+            newJoystick.PlayGameWithNewJoystick();
+
+            Console.WriteLine();
+
+            OldJoystick oldJoystick = new OldJoystick();
+            INewJoystick adaptedJoystick = new JoystickAdapter(oldJoystick);
+            adaptedJoystick.ConnectNewJoystick();
+            adaptedJoystick.PlayGameWithNewJoystick();
         }
     }
 }
